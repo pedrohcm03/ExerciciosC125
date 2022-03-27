4aa19c026955ad9130c7483604a81be201950c10
pedrohcm03 <pedrohcmiranda@get.inatel.br>
2022-03-27 20:48:31 -0300

    Exercício 1

---
+++ Zombicide1/src/main/java/Exercicio1.java
@@ -0,0 +1,13 @@
+public class Exercicio1 {
+    public static void main(String[] args){
+
+        int part1 = 150, part2 = 240, part3 = 320;
+
+        int total = part1 + part2 + part3;
+
+        int media = total/3;
+
+        System.out.println("Total de zumbis mortos: " + total);
+        System.out.println("Media de zumbis mortos: " + media);
+    }
+}
